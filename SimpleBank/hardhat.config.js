8bd7cde202ed433650bac73e6c3d967861712659
Nursca <margretnursca@gmail.com>
2025-10-30 14:06:04 +0100

    Initial Commit

---
+++ SimpleBank/hardhat.config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    baseSepolia: {
+      url: "https://sepolia.base.org", 
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
